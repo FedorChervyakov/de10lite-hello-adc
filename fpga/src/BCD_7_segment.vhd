39e0bf2b7faf6b9aaa7a08ee383c2bfc8731496b
Fedor Chervyakov <fchervyakov@gmail.com>
2020-11-07 21:55:22 +0300

    fpga: src: BCD_7_segment: change reset behaviour to blank the display
    Instead of displaying zero under reset, blank the display.

--- fpga/src/BCD_7_segment.vhd
+++ fpga/src/BCD_7_segment.vhd
@@ -2,7 +2,6 @@
 -- BCD digit to 7-segment display signals
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
 
 
 entity BCD_7_segment is
@@ -19,32 +18,29 @@ end entity;
 
 architecture A of BCD_7_segment is
     signal A, B, C, D : std_logic;
+    signal seven_sig_temp: std_logic_vector(6 downto 0);
 begin
-    A <= BCD(3) when reset='0' else
-         '0'    when reset='1' else
-         'X';
-    B <= BCD(2) when reset='0' else
-         '0'    when reset='1' else
-         'X';
-    C <= BCD(1) when reset='0' else
-         '0'    when reset='1' else
-         'X';
-    D <= BCD(0) when reset='0' else
-         '0'    when reset='1' else
-         'X';
+    A <= BCD(3);
+    B <= BCD(2);
+    C <= BCD(1);
+    D <= BCD(0);
     
     -- top horizontal bar
-    seven_sig(0) <= not ((not B and not D) or C or (B and D) or A);
+    seven_sig_temp(0) <= not ((not B and not D) or C or (B and D) or A);
     -- upper right vertical bar
-    seven_sig(1) <= not (not B or (not C and not D) or (C and D));
+    seven_sig_temp(1) <= not (not B or (not C and not D) or (C and D));
     -- lower right vertical bar
-    seven_sig(2) <= not (not C or D or B);
+    seven_sig_temp(2) <= not (not C or D or B);
     -- bottom horizontal bar
-    seven_sig(3) <= not ((not B and not D) or (not B and C) or (B and not C and D) or (C and not D) or A);
+    seven_sig_temp(3) <= not ((not B and not D) or (not B and C) or (B and not C and D) or (C and not D) or A);
     -- lower left vertical bar
-    seven_sig(4) <= not ((not B and not D) or (C and not D));
+    seven_sig_temp(4) <= not ((not B and not D) or (C and not D));
     -- upper left vertical bar
-    seven_sig(5) <= not ((not C and not D) or (B and not C) or (B and not D) or A);
+    seven_sig_temp(5) <= not ((not C and not D) or (B and not C) or (B and not D) or A);
     -- middle horizontal bar
-    seven_sig(6) <= not ((not B and C) or (B and not C) or A or (B and not D));
+    seven_sig_temp(6) <= not ((not B and C) or (B and not C) or A or (B and not D));
+
+    seven_sig <= seven_sig_temp  when reset='0' else
+                 (others => '1') when reset='1' else
+                 (others => 'X');
 end architecture A;
