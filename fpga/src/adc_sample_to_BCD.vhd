e4dfaf6d75f511fc4612cc26acc96b8fedd11bc7
Fedor Chervyakov <fchervyakov@gmail.com>
2020-11-09 00:43:56 +0300

    fpga: src: adc_sample_to_BCD: add voltage conversion to BCD digits

--- fpga/src/adc_sample_to_BCD.vhd
+++ fpga/src/adc_sample_to_BCD.vhd
@@ -6,13 +6,25 @@ use ieee.numeric_std.all;
 
 entity adc_sample_to_BCD is
     port (
-        adc_sample : in std_logic_vector(11 downto 0);
-        vol        : out std_logic_vector(12 downto 0)
+        adc_sample  : in std_logic_vector(11 downto 0);
+        vol         : out std_logic_vector(12 downto 0);
+        ones        : out std_logic_vector(3 downto 0);
+        tenths      : out std_logic_vector(3 downto 0);
+        hundredths  : out std_logic_vector(3 downto 0);
+        thousandths : out std_logic_vector(3 downto 0)
     );
 end entity;
 
 
 architecture A of adc_sample_to_BCD is
+    signal voltage : unsigned(vol'range);
 begin
-    vol <= std_logic_vector(resize(unsigned(adc_sample) * 2 * 2500 / 4095, 13));
+    voltage <= resize(unsigned(adc_sample) * 2 * 2500 / 4095, 13);
+
+    vol <= std_logic_vector(voltage);
+
+    ones <= std_logic_vector(resize(voltage / 1000, 4));
+    tenths <= std_logic_vector(resize((voltage / 100) - ((voltage / 1000) * 10), 4));
+    hundredths <= std_logic_vector(resize((voltage / 10) - ((voltage / 100) * 10), 4));
+    thousandths <= std_logic_vector(resize(voltage - ((voltage / 10) * 10), 4));
 end architecture A;
